c51f7de7d663344baf82cbb1f50f580b82ce3aac
alfersi <51697530+alfersi@users.noreply.github.com>
2024-02-03 18:34:45 +0100

    Update ch32v00x_iwdg.c
    And mask disambiguation

--- EVT/EXAM/SRC/Peripheral/src/ch32v00x_iwdg.c
+++ EVT/EXAM/SRC/Peripheral/src/ch32v00x_iwdg.c
@@ -92,7 +92,7 @@ void IWDG_ReloadCounter(void)
 void IWDG_Enable(void)
 {
     IWDG->CTLR = CTLR_KEY_Enable;
-    while ((RCC->RSTSCKR)& RCC_LSIRDY != RCC_LSIRDY);
+    while ((RCC->RSTSCKR & RCC_LSIRDY) != RCC_LSIRDY);
 }
 
 /*********************************************************************
